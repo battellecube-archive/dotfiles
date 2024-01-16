b3aa3c74c761c4c9de92a2187958d6ce34d3b390
Mike Sandman <sandmanm@battelle.org>
2024-01-16 09:36:09 -0500

    clean-up bootstrap to assume `make` has run

--- .local/bin/bootstrap-dotfiles.sh
+++ .local/bin/bootstrap-dotfiles.sh
@@ -1,71 +1,6 @@
 #!/usr/bin/env bash
 
-
-#!/bin/bash
-
 set -e
-
-################################################################################
-#
-#	DECLARE GLOBAL VARIABLES
-#
-################################################################################
-
-LOGFILE="$(mktemp --quiet)"
-
-
-################################################################################
-#
-#	DECLARE FUNCTIONS
-#
-################################################################################
-
-install-common-packages()
-{
-	sudo apt update -qq
-	sudo apt dist-upgrade -qq -y
-	sudo apt install -qq -y vim-nox build-essential gnupg curl git{,-lfs} etckeeper xdg-utils wslu software-properties-common
-	git lfs install
-}
-
-install-azure-cli() 
-{
-	type -p curl >>$LOGFILE || install-common-packages
-	curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-	az cloud set --name AzureUSGovernment
-	az config set extension.use_dynamic_install=yes_without_prompt
-}
-
-install-terraform-cli()
-{
-	type -p curl gpg >>$LOGFILE || install-common-packages
-	curl -sSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/hashicorp-archive-keyring.gpg
-	sudo chmod go+r /usr/share/keyrings/hashicorp-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/terraform-cli.list > /dev/null
-	sudo apt update -qq && sudo apt install terraform -yqq
-}
-
-install-github-cli()
-{
-	type -p curl >>$LOGFILE || install-common-packages
-	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-	sudo apt update -qq && sudo apt install gh -yqq
-}
-
-install-cube-cli()
-{
-	type -p gpg >>$LOGFILE || install-common-packages
-	type -p git >>$LOGFILE || install-common-packages
-	curl -sSL https://battellecube.github.io/cube-env/KEY.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/cubeenvcli-archive-keyring.gpg
-	sudo chmod go+r /usr/share/keyrings/cubeenvcli-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/cubeenvcli-archive-keyring.gpg] https://battellecube.github.io/cube-env ./" | sudo tee /etc/apt/sources.list.d/cube-env.list > /dev/null
-	echo "deb-src [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/cubeenvcli-archive-keyring.gpg] https://battellecube.github.io/cube-env ./" | sudo tee -a /etc/apt/sources.list.d/cube-env.list > /dev/null
-	sudo apt update -qq
-	sudo apt install -yqq cube-env
-	sudo apt build-dep -yqq cube-env
-}
 
 
 ################################################################################
@@ -87,41 +22,6 @@ else
         exit 1
     fi
 fi
-
-# remove lingering lists in case script was run previously and failed
-sudo rm -f /etc/apt/sources.list.d/{github-cli.list,terraform-cli.list,cube-env.list}
-
-################################################################################
-#
-#	INSTALL	PACKAGES
-#
-################################################################################
-
-
-install-common-packages
-
-echo -n "Installing Github CLI..."
-install-github-cli &>>$LOGFILE
-echo "done."
-
-echo -n "Install Terraform CLI..."
-install-terraform-cli &>>$LOGFILE
-echo "done."
-
-echo -n "Install Azure CLI..."
-install-azure-cli &>>$LOGFILE
-echo "done."
-
-gh auth status &>>$LOGFILE || {
-	echo "Need to login into gh to continue"
-	gh auth login
-}
-
-echo -n "Installing CUBE CLI..."
-install-cube-cli #&>>$LOGFILE
-echo "done."
-
-############### end install-tools
 
 
 ####################
